7cec75e34d627479be9cf425fdbc22f0b3cebcdf
Tin <ttran79@rrc.ca>
2024-06-21 17:29:16 -0500

    Added full_name validation and 5 customer  entries

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,5 @@
 class Customer < ApplicationRecord
+    validates :full_name, presence: true
     def self.ransackable_attributes(auth_object = nil)
         ["created_at", "email_address", "full_name", "id", "id_value", "notes", "phone_number", "updated_at"]
       end
